3552b036b71d6c0d38f709ddfa1cda58c26cf7a0
Vlad Flaks <fv@owox.com>
2025-01-21 10:16:25 -0500

    Facebook Marketing API v0.2

--- src/Integrations/FacebookMarketing/Connector.js
+++ src/Integrations/FacebookMarketing/Connector.js
@@ -1,135 +1,119 @@
 var FacebookMarketingConnector = class FacebookMarketingConnector extends AbstractConnector {
 
-  constructor( configRange ) {
+constructor( config ) {
+
+  super( config.mergeParameters({
+    AccessToken:{
+      isRequired: true,
+      requiredType: "string",
+    },
+    AccoundIDs: {
+      isRequired: true,
+    },
+    Fields: {
+      isRequired: true
+    },      
+    ReimportLookbackWindow: {
+      requiredType: "number",
+      isRequired: true,
+      default: 2
+    },
+    CleanUpToKeepWindow: {
+      requiredType: "number"
+    },
+    MaxFetchingDays: {
+      requiredType: "number",
+      isRequired: true,
+      default: 31
+    }
+  }));
   
-    super( configRange.mergeParameters({
-      AccessToken:{
-        isRequired: true,
-        requiredType: "string",
-      },
-      StartDate: {
-        isRequired: true,
-        requiredType: "date",
-        value: new Date(new Date().getFullYear(), new Date().getMonth(), 1)
-      },
-      EndDate: {
-        isRequired: true,
-        requiredType: "date",
-        value: new Date()
-      },
-      ReimportLookbackWindow: {
-        requiredType: "number",
-        isRequired: true,
-        value: 2
-      },
-      CleanUpToKeepWindow: {
-        requiredType: "number"
-      },
-      DestinationSheetName: {
-        isRequired: true,
-        value: "Data"
-      },
-      MaxFetchingDays: {
-        requiredType: "number",
-        isRequired: true,
-        value: 30
-      }
-    }));
-  
-    this.fieldsSchema = FacebookMarketingFieldsSchema;
-  
-  }
+  this.fieldsSchema = FacebookMarketingFieldsSchema;
+
+}
   
   /*
-  @param startDate start date
-  @param endDate end date
+  @param nodeName string
+  @param accountId string
+  @param fields array
+  @param startDate date
   
   @return data array
   
   */
-  fetchData(startDate, endDate)  {
+  fetchData(nodeName, accountId, fields, startDate = null)  {
   
-    var jsonURL = `https://graph.facebook.com/v19.0/act_550429885113645/insights?fields=date_stop,campaign_name,impressions,clicks,spend,frequency,action_values&period=day&date_preset=last_30d&time_increment=1&limit=500&level=campaign&access_token=${this.config.AccessToken.value}`;
-  
-    throw new Error(jsonURL);
+    console.log(`${nodeName}, ${accountId}, ${fields}, ${startDate}`);
+
+    let url = 'https://graph.facebook.com/v21.0/';
+
+    let formattedDate = null;
+    let timeRange = null;
+
+    if( startDate ) {
+      formattedDate = Utilities.formatDate(startDate, "UTC", "yyyy-MM-dd");
+      timeRange = encodeURIComponent(JSON.stringify({since:formattedDate, until:formattedDate}));
+    }
+
+    switch (nodeName) {
+      case 'ad-account':
+        url += `act_${accountId}?fields=${fields.join(",")}`;
+        break;
+
+      case 'ad-account-user':
+        url += `act_${accountId}/?fields=${fields.join(",")}`;
+        break;
+
+      case 'ad-account/insights':
+        url += `act_${accountId}/insights?level=ad&period=day&time_range=${timeRange}&fields=${fields.join(",")}`;
+        break;
+        // ad, adset, campaign, account
+
+      case 'ad-group':
+        url += `act_${accountId}/ads?&time_range=${timeRange}&fields=${fields.join(",")}`;
+        break;
+
+      default:
+        throw new Error(`End point for ${nodeName} is not implemented yet. Feel free add idea here: https://github.com/OWOX/js-data-connectors/discussions/categories/ideas`);
+    }
+
+    url += `&access_token=${this.config.AccessToken.value}`;
+
     var allData = [];
-    var nextPageURL = jsonURL;
+    var nextPageURL = url;
   
     while (nextPageURL) {
       // Fetch data from the JSON URL
+      //console.log(nextPageURL);
       var response = UrlFetchApp.fetch(nextPageURL);
+      
       var jsonData = JSON.parse(response.getContentText());
-      var data = jsonData.data;
-      nextPageURL = jsonData.paging ? jsonData.paging.next : null;
+
+      // This node point returns a result in the data property, which might be paginated 
+      if("data" in jsonData) {
+
+        nextPageURL = jsonData.paging ? jsonData.paging.next : null;
+
+        // date fields must be converted to Date objects to meet unique key requirements 
+        jsonData.data.forEach(record => {
+          if (record.date_start) record.date_start = new Date(record.date_start + "T00:00:00Z");
+          if (record.date_stop) record.date_stop = new Date(record.date_stop + "T00:00:00Z");
+        });
   
-      allData = allData.concat(data);
-    }
-    Logger.log(allData);
-  
-  
-  // time_range
-  //https://graph.facebook.com/v21.0/120215615997700036/insights?fields=account_id,ad_id,campaign_id,campaign_name,cost_per_action_type,video_p100_watched_actions,video_p25_watched_actions,impressions,clicks,spend&level=ad&date_preset=last_30d&access_token=EAAKNuiINZCvIBO0Le67cwetx0HXZBZBrFhHak61KSFtxeV6Smv3VssMs7hX9bZCLGnZC5anupDBXUCTouEQhOHu6hJxPkD1fhrAZC4n88mdPkrtrzXmueIV2ZAOHq4BcfWtO4Pf0PRYYqmWAtiRe3GRcxLcWjWktmTohQZARomCnJDZBdWG1WoINe6vIavN6vYf9uykmpjU8vOzXc4V6QoXuP9CxpKhrvIHjYgDGfTYkdgovjQQ7SAvNVnTZCvdhcw
-  
-    throw new Error('FetchData');
-    var data = [];
-    
-    //console.log(data);
-    return data;
-  
-  }
+        allData = allData.concat(jsonData.data);
+     
 
-/*
-
-returing two-levels object with schema details
-first level with name and description properties for group name
-the seconed one for the fields
-
-@return object
-
-*/
-getFieldsSchema() {
-
-  return this.getSigleLevelFieldsSchema(this.fieldsSchema);
-
-}
-
-/*
-
-recursive function to create one-level object schema with fields details
-
-@param object fieldsSchema
-@param string parent node name
-
-*/
-getSigleLevelFieldsSchema(fieldsSchema, parentGroupName = null) {
-
-  var schema = {};
-  let parentGroupPrefix = (parentGroupName ? parentGroupName + "/" : "");
-
-  for(var groupName in fieldsSchema) {
-
-    if( fieldsSchema[ groupName ].fields ) {
-      schema[ parentGroupPrefix + groupName ] = {
-        "isSelected":    parentGroupPrefix +  groupName,
-        "description":   fieldsSchema[ groupName ].description,
-        "documentation": fieldsSchema[ groupName ].documentation,
-        "fields":        fieldsSchema[ groupName ].fields,
-        //"fields":        Object.keys(fieldsSchema[ groupName ].fields).slice(0,5)
+      // this is non-paginated result
+      } else {
+        nextPageURL = null;
+        allData = allData.concat(jsonData);
       }
+      
     }
-
-    // there are some edges at this node, we need to add them to schema as well
-    if( fieldsSchema[ groupName ].edges ) {
-        schema = {
-          ...schema, 
-          ...this.getSigleLevelFieldsSchema( fieldsSchema[ groupName ].edges, parentGroupPrefix + groupName)
-        } 
-    }
-
+    //console.log(allData);
+    return allData;
+  
   }
 
-  return schema;
-
-}
 
 }
--- src/Integrations/FacebookMarketing/Pipeline.js
+++ src/Integrations/FacebookMarketing/Pipeline.js
@@ -1,4 +1,173 @@
 var FacebookMarketingPipeline = class FacebookMarketingPipeline extends AbstractPipeline {
 
 
+startImportProcess() {
+  
+  // Getting account IDs by splitting the configuration value by commas
+  let accountsIds = this.config.AccoundIDs.value.split(/[,;]\s*/);
+
+  // Getting an object of nodes whose fields array needs to be fetched from
+  let fields = this.config.Fields.value.split(", ").reduce( (acc, pair) => {
+    let [key, value] = pair.split(" ");
+    (acc[key] = acc[key] || []).push( value.trim() );
+    return acc;
+  }, {});
+
+  console.log(fields);
+
+  let timeSeriesNodes = {};
+  
+  // Data must be imported differently depending on whether it is time-series or not
+  for(var nodeName in fields) {
+    
+    // node’s data is time-series, so add the name of the node to timeSeriesNodes array to process it later
+    if(nodeName in this.connector.fieldsSchema
+      && "isTimeSeries" in this.connector.fieldsSchema[nodeName]
+      && this.connector.fieldsSchema[nodeName].isTimeSeries ) {
+     
+        timeSeriesNodes[nodeName] = fields[nodeName];
+
+    // node's data is catalog like, it must be imported right away
+    } else {
+
+      this.startImportProcessOfCatalogData(nodeName, accountsIds, fields[ nodeName ]);
+    
+    }
+
+  }
+
+  // if there are some time series nodes to import
+  if( timeSeriesNodes !== {} ) {
+    let startDate = null;
+    let daysToFetch = null;
+    [startDate, daysToFetch] = this.getStartDateAndDaysToFetch();
+
+    if( daysToFetch > 0 ) {
+      this.startImportProcessOfTimeSeriesData(accountsIds, timeSeriesNodes, startDate, daysToFetch);
+    }
+    
+  }
+
+  
+}
+
+/*
+
+Imports catalog (not time seriesed) data 
+
+@param nodeName string Node name
+@param accountsIds array list of account ids
+@param fields array list of fields 
+
+*/
+startImportProcessOfCatalogData(nodeName, accountIds, fields) {
+
+  for(var i in accountIds) {
+    
+    let accountId = accountIds[i];
+
+    let data = this.connector.fetchData(nodeName, accountId, fields);
+    
+    if( data.length ) {
+      this.config.logMessage(`${data.length} rows of ${nodeName} were fetched for account ${accountId}`);
+      this.getStorageByNode(nodeName, fields ).saveData( data );
+    }
+
+  }
+
+}
+
+/*
+
+Imports time series (not catalog) data 
+
+@param accountsIds (array) list of account ids
+@param timeSeriesNodes (object) of properties, each is array of fields
+@param startDate (Data) start date 
+@param daysToFetch (integer) days to import
+
+*/
+startImportProcessOfTimeSeriesData(accountsIds, timeSeriesNodes, startDate, daysToFetch = 1) {
+
+  // start requesting data day by day from startDate to startDate + MaxFetchingDays
+  for(var daysShift = 0; daysShift < daysToFetch; daysShift++) {
+
+    // itterating accounts  
+    for (let accountId of accountsIds) {
+   
+      // itteration nodes to fetch data
+      for(var nodeName in timeSeriesNodes) {
+
+        // fetching new data from a data source  
+        let data = this.connector.fetchData(nodeName, accountId, timeSeriesNodes[ nodeName ], startDate);
+
+          // there are fetched records to update
+        if( !data.length ) {      
+          
+          if( daysShift == 0) {
+            this.config.logMessage(`ℹ️ No records have been fetched for account ${accountId}`);
+          }
+
+        } else {
+
+          this.config.logMessage(`${data.length} rows were fetched for ${accountId}/${nodeName}/${Utilities.formatDate(startDate, "UTC", "yyyy-MM-dd")}`);
+          this.getStorageByNode(nodeName, timeSeriesNodes[ nodeName ] ).saveData(data);
+
+        }
+        
+      }
+    
+    }
+
+    this.config.updateLastRequstedDate(startDate);
+    startDate.setDate( startDate.getDate() + 1);  // let's move on to the next date
+
+  }    
+}
+
+/**
+ *
+ * @param nodeName string name of the node
+ * @param requestedFields array list of requested fields
+ * 
+ * @return AbstractStorage 
+ * 
+ */
+getStorageByNode(nodeName, requestedFields) {
+
+  // initiate blank object for storages
+  if( !("storages" in this) ) {
+    this.storages = {};
+  }
+
+  if( !(nodeName in this.storages) ) {
+
+    if( !("uniqueKeys" in this.connector.fieldsSchema[ nodeName ]) ) {
+      throw new Error(`Unique keys for '${nodeName}' are not defined in fields schema`);
+    }
+
+    let uniqueFields = requestedFields.filter(
+      a => this.connector.fieldsSchema[ nodeName ]["uniqueKeys"].includes(a)
+    );
+
+
+    this.storages[ nodeName ] = new GoogleSheetsStorage( 
+      this.config.mergeParameters({ DestinationSheetName: {value: nodeName} }), 
+      uniqueFields
+    );
+    
+    // if destination sheet is empty, we need to create header with columns for unique keys
+    if( this.storages[ nodeName ].isEmpty() ) {        
+      this.storages[ nodeName ].addHeader( this.storages[ nodeName ]["uniqueKeys"] );  // @TODO: this is needed for Google Sheets Storage only
+      this.config.logMessage(`Column(s) for unique key was added: ${this.uniqueKeyColumns}`);
+
+    // If the destination sheet already exists, we need to double-check that existing columns includes all requested unique keys
+    }
+  }
+
+  return this.storages[ nodeName ];
+
+}
+
+
 }

3e9b960a090047bce208fb280beba24decb263c2
Vlad Flaks <fv@owox.com>
2025-01-26 17:13:10 +0100

    Adding adAccount/Ads and adAccount/adCreatives support according to issue #9
    Adding adAccount/Ads and adAccount/adCreatives support

---
+++ src/Integrations/FacebookMarketing/MarketingAPIReference/ad-account-ads.js
@@ -0,0 +1,12 @@
+var adAccountAdsFields = {
+
+'insights': {
+  'description': 'Analytics summary for all objects',
+  'type': 'Edge<AdsInsights>'
+},
+'total_count': {
+  'description': 'Total number of Ads returned by the query',
+  'type': 'unsigned int32'
+}
+
+}

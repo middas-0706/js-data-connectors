4155aad91138680512fd179542b7269d6242c414
Vlad Flaks <fv@owox.com>
2025-01-27 22:26:50 +0100

    chore: delete unnecessary spaces

--- src/Core/AbstractConnector.js
+++ src/Core/AbstractConnector.js
@@ -7,36 +7,36 @@
 
 class AbstractConnector {
 
-  /**
-   * Asbstract class for Connectros
-   *
-   * @param {configSheetRange} instance of Range class with config data. The first row must be a name of the parameter, the second one its value 
-   * @param {configConnector} optional object with hardcoded config. Defined in inherited classes. configConnector data overwrites configSheetRange data 
-   *
-   */
-  constructor(config) {
-  
-    // Check if configRange is an instance of Range Class
-    if( typeof config.setParametersValues !== "function" ) {
-      throw new Error(`Unable to create an ${this.constructor.name} object. The first parameter must be inheritance of AbstractConfig class`)
-    } 
-  
-    this.config = config;
-  
-  }
-  
-  
-  /*
-  
-  A Data Source-specific methid is used to fetch new data and return it as an array of objects, where each property of an object corresponds to a column name.
-  
-  @return data array 
-  */
-  fetchData() {
-  
-    throw new Error("Method fetchData must be implemented in Class inheritor of AbstractConnector");
-  
-  }
+/**
+ * Asbstract class for Connectros
+ *
+ * @param {configSheetRange} instance of Range class with config data. The first row must be a name of the parameter, the second one its value 
+ * @param {configConnector} optional object with hardcoded config. Defined in inherited classes. configConnector data overwrites configSheetRange data 
+ *
+ */
+constructor(config) {
+
+  // Check if configRange is an instance of Range Class
+  if( typeof config.setParametersValues !== "function" ) {
+    throw new Error(`Unable to create an ${this.constructor.name} object. The first parameter must be inheritance of AbstractConfig class`)
+  } 
+
+  this.config = config;
+
+}
+
+
+/*
+
+A Data Source-specific methid is used to fetch new data and return it as an array of objects, where each property of an object corresponds to a column name.
+
+@return data array 
+*/
+fetchData() {
+
+  throw new Error("Method fetchData must be implemented in Class inheritor of AbstractConnector");
+
+}
 
 /*
 
